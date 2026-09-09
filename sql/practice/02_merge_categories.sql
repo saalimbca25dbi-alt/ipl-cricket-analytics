8b7e014fc4dd5c434fcf412791d594692c53539d
Muhammed Salim <saalim.bca25.dbi@gmail.com>
2026-09-09 15:45:40 +0530

    Add SQL Files

---
+++ sql/practice/02_merge_categories.sql
@@ -0,0 +1,24 @@
+DROP VIEW IF EXISTS v_deliveries_clean;
+
+CREATE VIEW v_deliveries_clean AS
+SELECT
+    *,
+    CASE
+        WHEN bowler_type = 'Fast medium'
+        THEN 'Fast Medium'
+        ELSE bowler_type
+    END AS bowler_type_clean
+FROM deliveries;
+
+
+DROP VIEW IF EXISTS v_teams_clean;
+
+CREATE VIEW v_teams_clean AS
+SELECT
+    *,
+    CASE
+        WHEN team_name = 'Rising Pune Supergiants'
+        THEN 'Rising Pune Supergiant'
+        ELSE team_name
+    END AS team_name_clean
+FROM teams;
